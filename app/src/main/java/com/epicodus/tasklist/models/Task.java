483bd7066b3d9229abc6638e55954d6d301ec1b1
the holla <holla@bikefarm.org>
2015-10-26 07:28:35 -0700

    Add behavior for Task class

--- app/src/main/java/com/epicodus/tasklist/models/Task.java
+++ app/src/main/java/com/epicodus/tasklist/models/Task.java
@@ -1,8 +1,43 @@
 package com.epicodus.tasklist.models;
 
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
+
+import java.util.List;
+
 /**
  * Created by admin on 10/26/15.
  */
-public class Task {
 
+// SQLite table should always be plural form of class name
+@Table(name = "Tasks", id = "_id")
+
+public class Task extends Model {
+    // provides two-way mapping to the database
+    @Column(name = "Description")
+    private String mDescription;
+
+    public Task() {
+        super();
+    }
+
+    public Task(String description) {
+        super();
+        mDescription = description;
+    }
+
+    public String getmDescription() {
+        return mDescription;
+    }
+
+    public void setmDescription(String description) {
+        mDescription = description;
+    }
+
+    public static List<Task> all() {
+        // Active Android translates this query into SQL, sends data, converts into objects, returns
+        return new Select().from(Task.class).execute();
+    }
 }
