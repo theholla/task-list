4f4495b5d6f94ce91e71c9825e43fe330e762843
the holla <holla@bikefarm.org>
2015-10-26 07:11:55 -0700

    Add behavior to temporarily store new tasks in task list

--- app/src/main/java/com/epicodus/tasklist/MainActivity.java
+++ app/src/main/java/com/epicodus/tasklist/MainActivity.java
@@ -1,37 +1,49 @@
 package com.epicodus.tasklist;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends ListActivity {
+    private ArrayList<String> mTasks;
+    private Button mNewTaskutton;
+    private EditText mNewTaskText;
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNewTaskutton = (Button) findViewById(R.id.newTaskButton);
+        mNewTaskText = (EditText) findViewById(R.id.newTask);
+
+        mTasks = new ArrayList<String>();
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mTasks);
+        // use this adapter to populate the list:
+        setListAdapter(mAdapter);
+
+        mNewTaskutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addTask();
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void addTask() {
+        String newTask = mNewTaskText.getText().toString();
+        mTasks.add(newTask);
+        // signal to adapter that a task is added so it can rerender the ListView
+        mAdapter.notifyDataSetChanged();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
